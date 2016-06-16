39a021174a3bc163e2bb674b0c35e6a2295dfbd8
Morgan Caron <morgan.caron@hetic.net>
2016-06-16 04:39:43 +0200

    easier sender/receiver object

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -18,11 +18,11 @@ class MessageSerializer < ActiveModel::Serializer
 
   def recipient
       # return object.recipient if @current_user.is_admin?
-      attributes["recipient"] = {"username" => object.recipient.username, "id" => object.recipient.id}
+      attributes["recipient"] = object.recipient.id
   end
   def sender
       # return object.sender if @current_user.is_admin?
-      attributes["sender"] = {"username" => object.sender.username, "id" => object.sender.id}
+      attributes["sender"] =  object.sender.id
   end
 
 
